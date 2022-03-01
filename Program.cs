5bac6d253b407b0b34b4db7030564a36b3c444da
sclabi <andrea.sclabas@gmail.com>
2022-03-01 23:41:36 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//
+// Author: Andrea
+// Date  : 01/03/2022 
+//
+// Task: Write a c# program to check palindrome number.
+
+namespace PalindromeNumber
+{
+    internal class PalindromeNumber
+    {
+        /// <summary>
+        /// Entry point of the program
+        /// </summary>
+        private static void Main()
+        {
+            bool firstCycle = true;
+            string? input;
+            do
+            {
+                // Error message visible only from the second cycle
+                if (!firstCycle)
+                {
+                    Console.WriteLine("Invalid number! Retry or digit EXIT to close.");
+                }
+
+                // Ask how many numbers print
+                Console.Write("Number to check: ");
+                input = Console.ReadLine();
+
+                // Exit condition
+                if (input == "EXIT")
+                    return;
+
+                firstCycle = false;
+
+                // Exit if a valid number has been provided
+            } while (!int.TryParse(input, out _));
+
+            // Obtain a reversed array of chars of the input value
+            char[] reverseNumber = input.ToArray();
+            Array.Reverse(reverseNumber);
+
+            // If the string created with the inverted array is equal to the starting string then the number is palindrome 
+            if (new string(reverseNumber) == input)
+            {
+                Console.WriteLine("Palindrome number");
+            }
+            else
+            {
+                Console.WriteLine("Non-palindrome number");
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## Fibonacci Series
+
+This simple program prints Fibonacci series in 2 different ways: without using recursion and using recursion.
